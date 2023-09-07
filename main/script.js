1da3a33838a109dc1003ee0dcbe4e5e2440a6472
DZXX21 <91760460+DZXX21@users.noreply.github.com>
2023-09-07 13:01:17 +0300

    tik bug fixlendi

--- main/script.js
+++ main/script.js
@@ -1,65 +1,91 @@
+// HTML içindeki elementleri alma
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// Görev eklemek için fonksiyon
 function addTask() {
-    if (inputBox.value == '') {
+    const taskText = inputBox.value.trim();
+    
+    if (taskText === "") {
         alert("You must write something!");
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = `
-            <input type="checkbox">
-            <label>${inputBox.value}</label>
-            <input type="text">
-            <button class="edit" onclick="editTask(this)">Edit</button>
-            <button class="delete" onclick="deleteTask(this)">Delete</button>
-        `;
-        listContainer.appendChild(li);
-        inputBox.value = "";
-        saveData();
+        return;
     }
+    
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+        <label>${taskText}</label>
+        <input type="text" class="edit-input">
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
+    `;
+    
+    listContainer.appendChild(listItem);
+    inputBox.value = "";
+    saveData();
 }
 
+// Liste öğelerine tıklama işlevi
 listContainer.addEventListener("click", function (e) {
-    if (e.target.tagName === "INPUT") {
-        e.target.parentElement.classList.toggle("checked");
+    const clickedElement = e.target;
+    
+    if (clickedElement.tagName === "INPUT" || clickedElement.tagName === "LABEL") {
+        const listItem = clickedElement.parentElement;
+        listItem.classList.toggle("checked");
         saveData();
     }
-}, false);
+});
 
+// Verileri localStorage'e kaydetme
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
+// Kaydedilmiş verileri gösterme
 function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
+    const savedData = localStorage.getItem("data");
+    if (savedData) {
+        listContainer.innerHTML = savedData;
+    }
 }
 
 showTask();
 
+// Görev düzenleme işlevi
+listContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("edit")) {
+        editTask(e.target);
+    }
+});
+
 function editTask(button) {
-    console.log("Edit Task...");
-    console.log("Change 'edit' to 'save'");
+    const listItem = button.parentElement;
+    const editInput = listItem.querySelector('input.edit-input');
+    const label = listItem.querySelector("label");
+    const isEditing = listItem.classList.contains("editMode");
 
-    var listItem = button.parentElement;
-
-    var editInput = listItem.querySelector('input[type=text]');
-    var label = listItem.querySelector("label");
-    var containsClass = listItem.classList.contains("editMode");
-    // If class of the parent is .editmode
-    if (containsClass) {
-        // Switch to .editmode
-        // Label becomes the input's value.
+    if (isEditing) {
+        // Düzenleme modundan çık
         label.innerText = editInput.value;
+        button.innerText = "Edit";
     } else {
+        // Düzenleme moduna gir
         editInput.value = label.innerText;
+        button.innerText = "Save";
     }
 
-    // Toggle .editmode on the parent.
     listItem.classList.toggle("editMode");
+    saveData();
 }
 
+// Görevi silme işlevi
+listContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("delete")) {
+        deleteTask(e.target);
+    }
+});
+
 function deleteTask(button) {
-    var listItem = button.parentElement;
+    const listItem = button.parentElement;
     listItem.remove();
     saveData();
 }
