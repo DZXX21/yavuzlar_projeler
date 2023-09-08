832a7cae92c86c1a8b2d93399fde13363cf8f6e9
DZXX21 <91760460+DZXX21@users.noreply.github.com>
2023-09-08 17:46:19 +0300

    biiti

---
+++ text/main.js
@@ -0,0 +1,100 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+    const searchInput = document.querySelector("#search-task-input");
+    const searchButton = document.querySelector("#search-task-button");
+
+    // Sayfa yüklendiğinde kaydedilmiş görevleri al
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    // Kaydedilmiş görevleri listeye ekle
+    savedTasks.forEach(savedTask => {
+        const task_el = createTaskElement(savedTask);
+        list_el.appendChild(task_el);
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+        const task_el = createTaskElement(task);
+
+        // Yeni görevi kaydet
+        savedTasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(savedTasks));
+
+        list_el.appendChild(task_el);
+
+        input.value = '';
+    });
+
+    // Arama işlevselliği
+    searchButton.addEventListener('click', () => {
+        const searchTerm = searchInput.value.toLowerCase();
+        const tasks = document.querySelectorAll('.task');
+
+        tasks.forEach((task) => {
+            const taskText = task.querySelector('.text').value.toLowerCase();
+            if (taskText.includes(searchTerm)) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            }
+        });
+    });
+
+    function createTaskElement(taskText) {
+        const task_el = document.createElement('div');
+        task_el.classList.add('task');
+
+        const task_content_el = document.createElement('div');
+        task_content_el.classList.add('content');
+
+        task_el.appendChild(task_content_el);
+
+        const task_input_el = document.createElement('input');
+        task_input_el.classList.add('text');
+        task_input_el.type = 'text';
+        task_input_el.value = taskText;
+        task_input_el.setAttribute('readonly', 'readonly');
+
+        task_content_el.appendChild(task_input_el);
+
+        const task_actions_el = document.createElement('div');
+        task_actions_el.classList.add('actions');
+
+        const task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerText = 'Edit';
+
+        const task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerText = 'Delete';
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el);
+
+        task_edit_el.addEventListener('click', (e) => {
+            if (task_edit_el.innerText.toLowerCase() == "edit") {
+                task_edit_el.innerText = "Save";
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+            } else {
+                task_edit_el.innerText = "Edit";
+                task_input_el.setAttribute("readonly", "readonly");
+            }
+        });
+
+        task_delete_el.addEventListener('click', (e) => {
+            list_el.removeChild(task_el);
+            // Görevi kaldır
+            savedTasks.splice(savedTasks.indexOf(taskText), 1);
+            localStorage.setItem('tasks', JSON.stringify(savedTasks));
+        });
+
+        return task_el;
+    }
+});
