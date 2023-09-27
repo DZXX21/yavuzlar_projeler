5a2230f755b881b3669d54cf2370d64d2670d62c
enverogulcan <48012114+enverogulcan@users.noreply.github.com>
2023-09-27 22:26:57 +0300

    s
    d

--- denem4/giris.php
+++ denem4/giris.php
@@ -6,31 +6,32 @@ $username = "root";
 $password = "";
 $database = "todolist";
 
+$conn = new mysqli($host, $username, $password, $database);
 
-$conn = mysqli_connect($host, $username, $password, $database);
-
-
-if (!$conn) {
-    die("Bağlantı hatası: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Bağlantı hatası: " . $conn->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $kullanici_adi = mysqli_real_escape_string($conn, $_POST["kullanici_adi"]);
-    $sifre = mysqli_real_escape_string($conn, $_POST["sifre"]);
-    
-    
-    $sql = "SELECT * FROM kullanicilar WHERE kullanici_adi='$kullanici_adi' AND sifre='$sifre'";
-    $result = $conn->query($sql);
-    
-    if ($result->num_rows > 0) {
-        
-        $_SESSION["kullanici_adi"] = $kullanici_adi; 
-        header("Location: ana_sayfa.php"); 
+    $kullanici_adi = $_POST["kullanici_adi"];
+    $sifre = $_POST["sifre"];
+
+    // Use prepared statement to prevent SQL injection
+    $sql = "SELECT kullanici_adi, sifre FROM kullanicilar WHERE kullanici_adi=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $kullanici_adi);
+    $stmt->execute();
+    $stmt->bind_result($db_kullanici_adi, $db_sifre);
+    $stmt->fetch();
+
+    if (password_verify($sifre, $db_sifre)) {
+        $_SESSION["kullanici_adi"] = $kullanici_adi;
+        $stmt->close();
+        $conn->close();
+        header("Location: ana_sayfa.php");
+        exit();
     } else {
-      
         echo "Kullanıcı adı veya şifre hatalı.";
     }
 }
-
-
 ?>
